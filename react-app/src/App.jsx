56fc117de07ce2654c1d8a17033a5f0cd046eba3
VladyslavKulichenko <kulichenko.vladyslav.v@gmail.com>
2025-04-12 16:42:43 +0300

    Installing and configuring ReactJS and TailwindCSS via Vite

---
+++ react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Vite + React App using TailwindCSS</h1>
+    </>
+  )
+}
+
+export default App
