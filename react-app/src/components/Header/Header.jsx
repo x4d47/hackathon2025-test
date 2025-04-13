ad31b4a8b6b89b489edf4d4d37749426b6953d2a
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-04-13 18:56:52 +0300

    Fix header
    Fixed account link

--- react-app/src/components/Header/Header.jsx
+++ react-app/src/components/Header/Header.jsx
@@ -11,42 +11,16 @@ import { useSelector } from "react-redux";
 import Logo from "../Logo/Logo.jsx";
 
 export default function Header({ children, ...props }) {
-	const location = useLocation();
-	const currentPath = location.pathname.slice(1);
-	const [isSearch, setIsSearch] = useState(false);
-	const [isProfile, setIsProfile] = useState(false);
-
 	const userName = useSelector((state) => state.auth.user.name);
-	// const isAuth = useSelector((state) => state.isAuthorizated);
-
-
-	useEffect(() => {
-		setIsSearch(() => {
-			if (currentPath === "search") {
-				return true;
-			} else {
-				return false;
-			}
-		});
-		setIsProfile(() => {
-			if (currentPath === "home") {
-				return true;
-			} else {
-				return false;
-			}
-		});
-	}, [currentPath]);
 
 	return (
 		<header className="header" {...props}>
 			<div className="header-wrapper container">
 				<Logo />
 				<ul className="links">{children}</ul>
-				{isProfile && (
-					<Link to="/profile" className="header__profile_link">
-						{userName || "Login or Register"} &#8599;
-					</Link>
-				)}
+				<Link to="/profile" className="header__profile_link">
+					{userName || "Login or Register"} &#8599;
+				</Link>
 			</div>
 		</header>
 	);
