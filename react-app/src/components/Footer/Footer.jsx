03c6c2979552cae56476cc3adc0c9dbe641af35e
VladyslavKulichenko <kulichenko.vladyslav.v@gmail.com>
2025-04-13 13:50:26 +0300

    Footer and Home Changes

--- react-app/src/components/Footer/Footer.jsx
+++ react-app/src/components/Footer/Footer.jsx
@@ -5,18 +5,10 @@ export default function Footer() {
 	return (
 		<>
 			<footer className="footer">
-				<div className="footer-wrapper container">
-					<div className=" grid grid--3-col main margin-btm-sm">
-						<div className="main_descrition">
-							<h3 className="heading-tertiary margin-btm-sm">Branding stuff</h3>
-							<p className="paragraph">
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-								Praesentium illum et asperiores minima tenetur sapiente
-								exercitationem labore ipsa
-							</p>
-						</div>
+				<div className="footer-wrapper">
+					<div className="main">
 						<div className="main_logo">
-							<img src="../public/bank.svg" alt="logo" width="64px" />
+							<img src="../public/bank.svg" alt="logo" width="54px" />
 						</div>
 					</div>
 					<div className="copyright">
